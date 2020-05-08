f4fe336475a5d79862cad32203c188b3a57abb46
kay.jan@hotmail.com <kay.jan@hotmail.com>
2020-05-08 19:33:41 +0800

    add css

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+
+app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+	html.Div([
+		html.H1('Title of banner')
+		],
+		className='banner'),
+	html.Div(),
+	html.Div()
+	])
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run_server(debug=True)
