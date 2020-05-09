f06d007bd5eb091a1ddc4375f7a507eaa1741d1b
kay.jan@hotmail.com <kay.jan@hotmail.com>
2020-05-10 00:50:09 +0800

    added map

--- app.py
+++ app.py
@@ -1,72 +1,120 @@
 import dash
-import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_leaflet as dl
 
+from dash.dependencies import Input, Output, State
+from tab_layout import about_me_tab, keyboard_tab, trip_tab
 
 app = dash.Dash(__name__)
+app.title = 'wow'
+app.config.suppress_callback_exceptions = True
 app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
 server = app.server
 
 app.layout = html.Div([
-
-	# Left banner
-	html.Div([
-	    html.Div(
-	        html.H1([
-	        	'KJ Wong'
-	        ],
-	            style={
-	               	'font-family': 'Kalam',
-	               	'color': 'white'
-	            }
-	        ),
-	        style={
-	        	'margin-top': '15vh',
-	        	'margin-left': '10px',
-	        }
-	    ),
-	    html.Div(
-	    	dcc.Tabs(
-	    		id='tabs-parent',
-	    		value='tab-1',
-	    		vertical=True,
-	    		parent_className='custom-tabs',
+    # Left contents
+    html.Div([
+        html.Div(
+            html.H1([
+                'KJ Wong'
+            ],
+                style={
+                    'color': 'white'
+                }
+            ),
+            style={
+                'margin-top': '15vh',
+            }
+        ),
+        html.Div(
+            dcc.Tabs(
+                id='tabs-parent',
+                value=None,
+                vertical=True,
+                parent_className='custom-tabs-parent',
+                className='custom-tabs',
                 children=[
-                     dcc.Tab(label='About Me', value='tab-1', className='custom-tab'),
-                     dcc.Tab(label='Keyboard (WIP)', value='tab-2', className='custom-tab'),
-                     dcc.Tab(label='Other fun stuff (WIP)', value='tab-3', className='custom-tab'),
+                    dcc.Tab(label='About Me', value='tab-1', className='custom-tab'),
+                    dcc.Tab(label='Keyboard', value='tab-2', className='custom-tab'),
+                    dcc.Tab(label='Trip Planner', value='tab-3', className='custom-tab'),
                 ],
                 colors={
-                	'background': '#89CFF0'
+                    'background': '#005b96'
                 },
-                style={
-                	'width': '100%'
-                }
-            )
-        )],
-		style={
-			'display': 'inline-block',
-			'width': '22vw',
-			'height': '97vh',
-			'background-color': '#89CFF0',
-		}),
+                persistence=True,
+                persistence_type='session'
+            ),
+        )
+    ],
+        style={
+            'position': 'fixed',
+            'display': 'inline-block',
+            'width': '22vw',
+            'height': '97vh',
+        },
+        className='sidebar'),
 
-	# Right contents
-	html.Div([
-			'main body'
-		],
-			id='tab-content',
-			style={
-				'width': '76vw',
-				'height': '97vh',
-				'float': 'right',
-				'margin-left': '1px',
-				'border': '1px solid black'
-			})
-	])
+    # Right contents
+    html.Div(
+        id='tab-content',
+        style={
+            'display': 'inline-block',
+            'width': '76vw',
+            'margin-left': '22vw',
+        }
+    ),
+
+    # Hidden contents
+    html.Div([
+        html.Div(id='placeholder')
+    ],
+        style={
+            'display': 'none'
+        }
+    )
+])
+
+
+@app.callback(Output('placeholder', 'children'),
+              [Input('button_music', 'n_clicks')])
+def play(trigger):
+    if trigger:
+        import base64
+        sound_filename = 'assets/Music_Note/sample_sound.wav'  # replace with your own .mp3 file
+        # sound_filename = 'assets/Music_Note/C.wav'  # replace with your own .mp3 file
+        encoded_sound = base64.b64encode(open(sound_filename, 'rb').read())
+        return html.Audio(src=f'data:audio/wav;base64,{encoded_sound.decode()}', controls=False)
+
+
+@app.callback(Output('graph-map', 'children'),
+              [Input('graph-map', 'click_lat_lng')],
+              [State('input_trip_landmark', 'value'),
+               State('graph-map', 'children')])
+def click_coord(e, landmark, children):
+    if e is not None:
+        children.append(
+            # Marker icon (dict) can contain iconUrl ("/assets/images/mapbox-icon.png") and iconSize ([25, 25])
+            # Marker children (list) can contain dl.Tooltip() and dl.Popup()
+            dl.Marker(
+                position=[e[0], e[1]],
+                children=[
+                    dl.Tooltip(landmark),
+                ]))
+    return children
+
+
+@app.callback(Output('tab-content', 'children'),
+              [Input('tabs-parent', 'value')])
+def update_output(tab):
+    if tab == 'tab-1':
+        return about_me_tab()
+    elif tab == 'tab-2':
+        return keyboard_tab()
+    elif tab == 'tab-3':
+        return trip_tab()
 
 
 if __name__ == '__main__':
-	app.run_server(debug=True)
+    app.run_server(debug=True)
