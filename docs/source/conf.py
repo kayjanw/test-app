f633c825477e597a98ebb7410cd91e0dcfc0fdd4
kay.jan@hotmail.com <kay.jan@hotmail.com>
2020-05-27 12:13:40 +0800

    change documentation to rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon']
+extensions = ['sphinx.ext.napoleon', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -53,12 +53,17 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-    'leftsidebar': 'true',
-    'sidebarbgcolor': '#202029',
-    'relbarbgcolor': '#202029',
+    'collapse_navigation': False
 }
+
+# html_theme = 'classic'
+# html_theme_options = {
+#     'leftsidebar': 'true',
+#     'sidebarbgcolor': '#202029',
+#     'relbarbgcolor': '#202029',
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
