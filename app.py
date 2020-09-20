ba482beb3daf544ff186c883dfe08fc05f7c2d08
KJ <31106307+kayjanw@users.noreply.github.com>
2020-09-20 15:22:56 +0800

    reorder google tags

--- app.py
+++ app.py
@@ -55,14 +55,6 @@ app.index_string = '''
 <!DOCTYPE html>
 <html>
     <head>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MTFTGR8');</script>
-    <!-- End Google Tag Manager -->
-
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178463864-1"></script>
     <script>
@@ -71,6 +63,14 @@ app.index_string = '''
         gtag('js', new Date());
         gtag('config', 'UA-178463864-1');
     </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MTFTGR8');</script>
+    <!-- End Google Tag Manager -->
         {%metas%}
         <title>{%title%}</title>
         {%favicon%}
