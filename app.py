fd5954e84183bb0fe43e929c634b5ae844c80f9b
KJ <31106307+kayjanw@users.noreply.github.com>
2020-09-20 21:41:54 +0800

    remove gtm

--- app.py
+++ app.py
@@ -63,24 +63,12 @@ app.index_string = '''
         gtag('js', new Date());
         gtag('config', 'UA-178463864-1');
     </script>
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MTFTGR8');</script>
-    <!-- End Google Tag Manager -->
         {%metas%}
         <title>{%title%}</title>
         {%favicon%}
         {%css%}
     </head>
     <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MTFTGR8"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
         {%app_entry%}
         <footer>
             {%config%}
