d8f62d8254439e8a760a2d18ba922358845a911a
Kay Jan WONG <kay.jan@hotmail.com>
2022-01-09 21:30:15 +0800

    nltk download package, fix unittest

--- components/mbti.py
+++ components/mbti.py
@@ -28,6 +28,7 @@ except ModuleNotFoundError:
     pass
 
 # import nltk
+# nltk.download('omw-1.4')
 # nltk.download('punkt')
 # nltk.download('wordnet')
 
---
+++ tests/test_install.py
@@ -0,0 +1,17 @@
+import unittest
+import warnings
+
+warnings.filterwarnings("ignore")
+warnings.simplefilter("ignore", ResourceWarning)
+
+
+class TestImport(unittest.TestCase):
+    def test_import(self):
+        import nltk
+        from nltk.corpus import stopwords, wordnet
+        stopwords.words("english")
+        try:
+            wordnet.ensure_loaded()
+        except AttributeError:
+            wordnet.ensure_loaded()
+        print("Successfully imported nltk")
