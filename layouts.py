5e4e3a867af1e81c4dee4aa60bb2c589559b6c86
Kay Jan WONG <kay.jan@hotmail.com>
2022-01-11 19:06:50 +0800

    v1.0.4: update about me tab

--- layouts.py
+++ layouts.py
@@ -219,6 +219,8 @@ def about_me_tab(app):
                         html.A("linkedin", href="https://www.linkedin.com/in/kayjan/", target="_blank"),
                         " / ",
                         html.A("formal website", href="http://kayjan.github.io/", target="_blank"),
+                        ". I write on Medium too! Follow me ",
+                        html.A("here", href="https://kayjanwong.medium.com/", target="_blank"),
                     ]
                 ),
                 html.P(
