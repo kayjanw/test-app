cdf7b86d5ec6c2f289d72a34cbeaa910c0cf003c
KJ <31106307+kayjanw@users.noreply.github.com>
2022-01-16 10:11:12 +0800

    Add buymeacoffee

--- layouts.py
+++ layouts.py
@@ -186,7 +186,14 @@ def about_me_tab(app):
         [
             content_header("About me"),
             html.Div([
-                html.P("Just someone trying to apply what I learn, and believes coding should make our lives easier"),
+                html.P(
+                    [
+                        "Just someone who loves coding, and believes coding should make our lives easier. ",
+                        "If you like this, "
+                        html.A("buy me a coffee", href="https://www.buymeacoffee.com/kayjan", target="_blank"),
+                        "!"
+                    ]
+                ),
                 html.Div(
                     [
                         html.Img(src=app.get_asset_url("data-analytics.svg")),
