dfaa4b998735d26e795ef34f83208e7766e4e832
KJ <31106307+kayjanw@users.noreply.github.com>
2022-01-16 10:36:34 +0800

    Add unit test

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import unittest
+import warnings
+
+warnings.filterwarnings("ignore")
+warnings.simplefilter("ignore", ResourceWarning)
+
+
+class TestApp(unittest.TestCase):
+    def test_app(self):
+        from app import app
+        print("Successfully imported app")
