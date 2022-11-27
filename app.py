ebd03bbb5dba76ebe993db258846b773f1ff9567
Kay Jan <kayjanw@gmail.com>
2022-11-27 23:34:01 +0800

    remove initial-scale=1 from viewport

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ meta_tags = [
     },
     {
         "name": "viewport",
-        "content": "width=device-width, initial-scale=1"
+        "content": "width=device-width"
     },
     {
         "property": "og:type",
