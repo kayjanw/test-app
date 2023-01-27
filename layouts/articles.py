212954f0474b7fa6fb4113cecf871e44b2a79ba9
Kay Jan <kayjanw@gmail.com>
2023-01-28 01:23:31 +0800

    v1.1.21 add articles, change article colour, reorder articles

--- layouts/articles.py
+++ layouts/articles.py
@@ -6,11 +6,13 @@ from dash_iconify import DashIconify
 from layouts.main import content_header
 
 topic_colour_dict = {
-    "Productivity": "lightblue",
-    "Dashboard": "blue",
-    "Data Science": "red",
+    "Productivity": "dark",
+    "Data Science (SL)": "red",
+    "Data Science (UL)": "orange",
+    "Data Science (RL)": "yellow",
     "Coding Best Practices": "green",
-    "Software Engineering": "yellow",
+    "Dashboard": "lightblue",
+    "Software Engineering": "blue",
 }
 
 
@@ -22,40 +24,34 @@ card_list = [
         "https://towardsdatascience.com/5-tips-after-completing-100-coursera-courses-1ee5a35e72a4?sk=bd67c6888a7a552c8f5fbdedf64f7554",
     ),
     (
-        "Dashboard",
-        "Advancing to Professional Dashboard with Python, using Dash",
-        "Advanced ways to enhance initial skeleton code.",
-        "https://towardsdatascience.com/advancing-to-professional-dashboard-with-python-using-dash-and-plotly-1e8e5aa4c668?sk=e021c653a51b578a8382828f68ce483e",
-    ),
-    (
-        "Data Science",
-        "6 Reinforcement Learning Algorithms Explained",
-        "Introduction to reinforcement learning terminologies, basics, and concepts.",
-        "https://towardsdatascience.com/6-reinforcement-learning-algorithms-explained-237a79dbd8e?sk=23d7245dbdaf0c0082edd579c623da8d",
-    ),
-    (
-        "Data Science",
+        "Data Science (SL)",
         "CatBoost vs. LightGBM vs. XGBoost",
         "Which is the best algorithm?",
         "https://towardsdatascience.com/catboost-vs-lightgbm-vs-xgboost-c80f40662924?sk=abe57d6a8c3965f6997b04970b6dda0e",
     ),
     (
-        "Data Science",
-        "7 Evaluation Metrics for Clustering Algorithms",
-        "In-depth explanation with Python examples of unsupervised learning evaluation metrics",
-        "https://towardsdatascience.com/7-evaluation-metrics-for-clustering-algorithms-bdc537ff54d2?sk=565cf6977935e51f1622e2b28ed8d8cc",
-    ),
-    (
-        "Data Science",
+        "Data Science (SL)",
         "Feature Encoding Techniques in Machine Learning with Python Implementation",
-        "6 feature encoding techniques to consider for your data science workflows",
+        "6 feature encoding techniques to consider for your data science workflows.",
         "https://towardsdatascience.com/feature-encoding-techniques-in-machine-learning-with-python-implementation-dbf933e64aa?sk=47d0a64ab3eed9bc3babf59870152aff",
     ),
     (
-        "Coding Best Practices",
-        "3 Tips for Writing Clean Codes Beyond Coding Best Practices",
-        "Write elegant, modular, understandable, and maintainable codes.",
-        "https://towardsdatascience.com/3-tips-for-writing-clean-codes-beyond-coding-best-practices-c53b04120c3?sk=f8e1323aeaee830874422abb6ad1dc69",
+        "Data Science (UL)",
+        "7 Evaluation Metrics for Clustering Algorithms",
+        "In-depth explanation with Python examples of unsupervised learning evaluation metrics.",
+        "https://towardsdatascience.com/7-evaluation-metrics-for-clustering-algorithms-bdc537ff54d2?sk=565cf6977935e51f1622e2b28ed8d8cc",
+    ),
+    (
+        "Data Science (UL)",
+        "Association Rule Mining in Unsupervised Learning",
+        "Pattern discovery terminologies and concepts in data mining.",
+        "https://towardsdatascience.com/association-rule-mining-in-unsupervised-learning-df86170160de?sk=6d7c2553b4f0414f7bfe23ebd08a2bc3",
+    ),
+    (
+        "Data Science (RL)",
+        "6 Reinforcement Learning Algorithms Explained",
+        "Introduction to reinforcement learning terminologies, basics, and concepts.",
+        "https://towardsdatascience.com/6-reinforcement-learning-algorithms-explained-237a79dbd8e?sk=23d7245dbdaf0c0082edd579c623da8d",
     ),
     (
         "Coding Best Practices",
@@ -65,9 +61,21 @@ card_list = [
     ),
     (
         "Coding Best Practices",
+        "3 Tips for Writing Clean Codes Beyond Coding Best Practices",
+        "Write elegant, modular, understandable, and maintainable codes.",
+        "https://towardsdatascience.com/3-tips-for-writing-clean-codes-beyond-coding-best-practices-c53b04120c3?sk=f8e1323aeaee830874422abb6ad1dc69",
+    ),
+    (
+        "Coding Best Practices",
         "Advanced Code Documentation Beyond Comments and Docstrings",
         "Use Sphinx and Read the Docs for a user-friendly interface to understand codebase — even for non-technical users.",
         "https://towardsdatascience.com/advanced-code-documentation-beyond-comments-and-docstrings-2cc5b2ace28a?sk=e27cf3ec7723a1af37271e83e4dff888",
+    ),
+    (
+        "Dashboard",
+        "Advancing to Professional Dashboard with Python, using Dash",
+        "Advanced ways to enhance initial skeleton code.",
+        "https://towardsdatascience.com/advancing-to-professional-dashboard-with-python-using-dash-and-plotly-1e8e5aa4c668?sk=e021c653a51b578a8382828f68ce483e",
     ),
     (
         "Software Engineering",
@@ -83,12 +91,6 @@ card_list = [
     ),
     (
         "Software Engineering",
-        "3 Data Structures for Faster Python Lists",
-        "Choose your lists wisely.",
-        "https://towardsdatascience.com/3-data-structures-for-faster-python-lists-f29a7e9c2f92?sk=85d35153dca175f49f9fe63cc1f7bb35",
-    ),
-    (
-        "Software Engineering",
         "Pytest with Marking, Mocking, and Fixtures in 10 Minutes",
         "Write robust unit tests with Python pytest.",
         "https://towardsdatascience.com/pytest-with-marking-mocking-and-fixtures-in-10-minutes-678d7ccd2f70?sk=9707ed3994d15e2e2a0344f4dfec425b",
@@ -98,6 +100,12 @@ card_list = [
         "Unit Testing with Mocking in 10 Minutes",
         "Test your codebase effectively with the built-in unittest Python package.",
         "https://towardsdatascience.com/unit-testing-with-mocking-in-10-minutes-e28feb7e530?sk=4a2f6e8cb1a99d626e70977a45d68b6f",
+    ),
+    (
+        "Software Engineering",
+        "3 Data Structures for Faster Python Lists",
+        "Choose your lists wisely.",
+        "https://towardsdatascience.com/3-data-structures-for-faster-python-lists-f29a7e9c2f92?sk=85d35153dca175f49f9fe63cc1f7bb35",
     ),
     (
         "Software Engineering",
@@ -147,6 +155,19 @@ button_dict = {
 
 
 def articles_tab():
+    # Button
+    button_div_list = []
+    for topic, colour in topic_colour_dict.items():
+        button_div_list.append(
+            dmc.Button(
+                topic,
+                id={"type": "button-article", "id": f"article-{topic}"},
+                className=f"background-{colour}",
+                **button_dict,
+            )
+        )
+
+    # Card
     card_div_list = []
     for idx, values in enumerate(card_list):
         topic, title, description, link = values
@@ -167,16 +188,6 @@ def articles_tab():
                 id={"type": "card-article", "id": f"article-{topic}", "idx": idx},
                 href=link,
                 target="_blank",
-            )
-        )
-    button_div_list = []
-    for topic, colour in topic_colour_dict.items():
-        button_div_list.append(
-            dmc.Button(
-                topic,
-                id={"type": "button-article", "id": f"article-{topic}"},
-                className=f"background-{colour}",
-                **button_dict,
             )
         )
 
