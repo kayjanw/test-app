d95aa64203f4752147e7a863b8e6c0db4856b234
Kay Jan <kayjanw@gmail.com>
2025-08-02 02:15:59 +0800

    books to have carousel display

--- cv/layouts/helper.py
+++ cv/layouts/helper.py
@@ -5,13 +5,35 @@ from dash import html
 from dash_iconify import DashIconify
 
 
-def rating_review(rating: Union[float, int], review: str):
+def rating_review(
+    rating: Union[float, int], review: str, carousel: bool = False
+) -> dmc.Group:
+    """Rating and review for table cell
+
+    Args:
+        rating: rating
+        review: review details
+        carousel: whether display is for carousel card
+    """
+    if carousel:
+        return html.Div(
+            [
+                dmc.Rating(fractions=3, value=rating, readOnly=True),
+                html.Span(review, className="span-book"),
+            ]
+        )
     return dmc.Group(
         [dmc.Rating(fractions=3, value=rating, readOnly=True), html.Span(review)]
     )
 
 
-def bullet_point(icon: str, text: Any):
+def bullet_point(icon: str, text: Any) -> html.Div:
+    """Bullet point for accordian details
+
+    Args:
+        icon: icon for bullet point
+        text: bullet point text
+    """
     return html.Div(
         [
             html.Span(icon, style={"marginRight": "0.5em"}),
@@ -24,7 +46,21 @@ def bullet_point(icon: str, text: Any):
 def accordian(
     details: List[List[Union[str, List[str]]]],
     value: Union[str, List[str]] = [],
-):
+) -> dmc.Accordion:
+    """Display details in accordian layout
+
+    Args:
+        details: details consisting of
+            - title
+            - subtitle
+            - accordian icon
+            - detail
+            - accordian id
+        value: value(s) of active accordian id
+
+    Returns:
+        Accordian item
+    """
     return dmc.Accordion(
         chevron=DashIconify(icon="ant-design:down-outlined"),
         chevronPosition="right",
@@ -52,7 +88,19 @@ def accordian(
     )
 
 
-def create_scrollable_area(data: List[List[str]], columns: List[str], **kwargs):
+def create_scrollable_area(
+    data: List[List[str]], columns: List[str], **kwargs
+) -> dmc.TableScrollContainer:
+    """Create scrollable table
+
+    Args:
+        data: data to show, each entry is a row
+        columns: column names of table
+        **kwargs: any table kwargs
+
+    Returns:
+        Scrollable table
+    """
     table_kwargs = dict(
         withTableBorder=False,
         withColumnBorders=False,
@@ -71,7 +119,17 @@ def create_scrollable_area(data: List[List[str]], columns: List[str], **kwargs):
     )
 
 
-def create_course_table(data: List[List[str]], columns: List[str]):
+def create_course_table(data: List[List[str]], columns: List[str]) -> List[Any]:
+    """Create a table of 2-3 columns, third column is optional and will display as
+    a button if it is an url.
+
+    Args:
+        data: data to show, each entry is a row
+        columns: column names of table
+
+    Returns:
+        Table data
+    """
     return [
         html.Thead(html.Tr([html.Th(col) for col in columns])),
         html.Tbody(
