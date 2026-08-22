79905617184c32c9bc8d49f47810f358e983cf2c
Kay Jan <kayjanw@gmail.com>
2026-08-22 23:48:49 +0800

    do not showdown when no more chips

--- main/callbacks/poker.py
+++ main/callbacks/poker.py
@@ -61,7 +61,7 @@ def register_callbacks_poker(app, print_function):
         if poker_game.player_moved and not poker_game.game_over:
             poker_game.cpu_move()
 
-        if poker_game.stage == STAGES[-1]:
+        if poker_game.stage == STAGES[-1] and not poker_game.game_over:
             poker_game.showdown()
 
         # Button display
