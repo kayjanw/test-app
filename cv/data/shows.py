253a5d161a22d15e31c12429f44fbf85d34f26bc
KJ <31106307+kayjanw@users.noreply.github.com>
2026-09-14 10:05:27 +0800

    Update shows.py

--- cv/data/shows.py
+++ cv/data/shows.py
@@ -102,7 +102,7 @@ theatres = [
         "2026-09-13 17:00",
         "The Blackbird Singapore (Gillman Barracks)",
         "General Admission",
-        Review("Mental health focused", 2.33)
+        Review("Mental health focused", 2.33),
     ),
 ]
 musicals = [
